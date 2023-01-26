661c3e0e1a3dee84bc7e76108074c1ebfee98dbc
assaf lello <assaflello30@gmail.com>
2023-01-26 20:29:18 +0200

    Initial info provided

--- Profile.java
+++ Profile.java
@@ -1,8 +1,9 @@
-public cass Profile{
-	public static void main(String [] args)
+public class Profile{
+	public static void main(String [] args){
 		System.out.println("Elias Assaf");
 		System.out.println("Mechatronics Engineering");
 		System.out.println("First Year");
+		
 }
 
 
